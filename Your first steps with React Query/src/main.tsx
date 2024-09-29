191882534d34f2030aa057dfe64e13c0f0c8bff9
udip-rai <atomicdeveloper0329@gmail.com>
2024-09-29 09:00:21 +0545

    feature - wrap main.tsx with react query context

--- Your first steps with React Query/src/main.tsx
+++ Your first steps with React Query/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+// Import - components
 import App from "./App.tsx";
+
+// Import - css
 import "./index.css";
 
+// Import - node_modules
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create an instance of query client
+const queryClient = new QueryClient();
+
+// Main
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
